86f03cc7a7004f08f981bda9402a1bab8a9ed3f0
PAVAN KALYAN PENNAPATI <68542761+Pa163361@users.noreply.github.com>
2023-02-19 08:54:45 +0530

    Add files via upload

--- models/note.js
+++ models/note.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI
+const url = 'mongodb+srv://fullstack:fullstackPavan1@firstcluster.legrnuq.mongodb.net/noteApp?retryWrites=true&w=majority'
 console.log('connecting to', url)
 mongoose.connect(url)
     .then(result => { console.log('connected to MongoDB') })
